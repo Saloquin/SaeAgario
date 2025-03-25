22df9f6e9a61969c14ea317c5af4b33228a70071
Xecozz <gillecamille@gmail.com>
2025-03-25 22:06:05 +0100

    refactor client side

--- src/main/java/com/example/sae/client/Solo.java
+++ src/main/java/com/example/sae/client/Solo.java
@@ -1,27 +1,18 @@
 package com.example.sae.client;
 
-import com.example.sae.client.controller.MenuController;
 import com.example.sae.core.GameEngine;
 import com.example.sae.core.entity.Enemy;
 import com.example.sae.core.entity.Food;
 import com.example.sae.core.entity.Player;
-import javafx.animation.AnimationTimer;
 import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 import static com.example.sae.core.GameEngine.MAP_LIMIT_HEIGHT;
 import static com.example.sae.core.GameEngine.MAP_LIMIT_WIDTH;
 
 public class Solo extends Client {
     private GameTimer gameTimer;
-    private int playerId;
 
     public Solo(Group root) {
         super(root);
@@ -37,7 +28,6 @@ public class Solo extends Client {
         camera.focusOn(player);
         playerId = gameEngine.addPlayer(player);
         gameTimer.start();
-
     }
 
     @Override
@@ -62,31 +52,7 @@ public class Solo extends Client {
             gameEngine.addEntity(new Enemy(root, 5));
         }
 
-
         gameEngine.update();
-    }
-
-    private static class GameTimer extends AnimationTimer {
-        private final Solo client;
-        private final double framesPerSecond = 120;
-        private final double interval = 1000000000 / framesPerSecond;
-        private double last = 0;
-
-        public GameTimer(Solo client) {
-            this.client = client;
-        }
-
-        @Override
-        public void handle(long now) {
-            if (last == 0) {
-                last = now;
-            }
-
-            if (now - last > interval && client.gameStarted) {
-                last = now;
-                client.update();
-            }
-        }
     }
 
     private void returnToMenu() {
