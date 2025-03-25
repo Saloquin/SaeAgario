5a31a02f53b2d98c3cbd9921ebd6244827555a87
AstatePNG <elsa.hmn76@gmail.com>
2025-03-25 11:02:29 +0100

    [BUILD CONFIG] add of dependencies (JUnit, Jacoco and Checkstyle) to the pom.xml file of the project to ensure code quality

---
+++ src/test/java/com/example/sae/core/quadtree/BoundaryTest.java
@@ -0,0 +1,31 @@
+package com.example.sae.core.quadtree;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BoundaryTest {
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void intersects() {
+    }
+
+    @Test
+    void getX() {
+    }
+
+    @Test
+    void getY() {
+    }
+
+    @Test
+    void getWidth() {
+    }
+
+    @Test
+    void getHeight() {
+    }
+}
