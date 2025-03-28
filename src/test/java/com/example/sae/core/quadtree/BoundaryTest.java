d719a2c00f67fb55ff672a7446a0f01e5110e665
AstatePNG <elsa.hmn76@gmail.com>
2025-03-28 09:45:05 +0100

    [TEST] add of tests to the boundary class

--- src/test/java/com/example/sae/core/quadtree/BoundaryTest.java
+++ src/test/java/com/example/sae/core/quadtree/BoundaryTest.java
@@ -1,31 +1,70 @@
 package com.example.sae.core.quadtree;
 
+import junit.framework.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class BoundaryTest {
 
-    @Test
-    void contains() {
+    private Boundary boundaryTest;
+
+    @BeforeEach
+    void setUp() {
+        boundaryTest = new Boundary(0.0,0.0,1.0,1.0);
+    }
+
+    private static Stream<Arguments> pointsDeliverer() {
+        return Stream.of(
+                Arguments.of(true, 0.5, 0.5),
+                Arguments.of(false, 0.5, -0.5),
+                Arguments.of(true, 0.0, 0.0)
+        );
+    }
+
+    private static Stream<Arguments> boundariesDeliverer() {
+        return Stream.of(
+                Arguments.of(true, new Boundary(0.5,0.5,0.2,0.2)),
+                Arguments.of(false, new Boundary(1.5,1.5,1.0,1.0)),
+                Arguments.of(true, new Boundary(0.0,0.0,1.0,1.0))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource( "pointsDeliverer" )
+    void containsTest(boolean target, double x, double y) {
+        assertEquals(target, boundaryTest.contains(x, y));
+    }
+
+    @ParameterizedTest
+    @MethodSource( "boundariesDeliverer" )
+    void intersectsTest(boolean target, Boundary boundary) {
+        assertEquals(target, boundaryTest.intersects(boundary));
     }
 
     @Test
-    void intersects() {
+    void getXTest() {
+        assertEquals(0.0,boundaryTest.getX());
     }
 
     @Test
-    void getX() {
+    void getYTest() {
+        assertEquals(0.0,boundaryTest.getY());
     }
 
     @Test
-    void getY() {
+    void getWidthTest() {
+        assertEquals(1.0,boundaryTest.getWidth());
     }
 
     @Test
-    void getWidth() {
-    }
-
-    @Test
-    void getHeight() {
+    void getHeightTest() {
+        assertEquals(1.0,boundaryTest.getHeight());
     }
 }
